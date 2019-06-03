703003f9f3c83498e0d3181a51d9659611dcb306
laopo001 <353272497@qq.com>
2019-06-03 22:05:16 +0800

    fix

--- src/bin/main4.rs
+++ src/bin/main4.rs
@@ -7,16 +7,16 @@ use hello_rust::test::ThreadPool;
 fn main() {
 	let pool = ThreadPool::new(4);
 	let (s, receiver) = mpsc::channel();
-	let s = Arc::new(Mutex::new(sender));
+	let sender = Arc::new(Mutex::new(sender));
 //	let sender = s.clone();
 	pool.execute(move || {
 		sleep(std::time::Duration::from_secs(2));
-		sender.send("123".to_string());
+		sender.lock().unwrap().send("123".to_string());
 	});
 	let sender = s.clone();
 	pool.execute(move || {
 		sleep(std::time::Duration::from_secs(2));
-		sender.send("123567567".to_string());
+		sender.lock().unwrap().send("123567567".to_string());
 	});
 
 	loop {
