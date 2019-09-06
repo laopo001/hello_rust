5935a5a2f577af654686e3c9e5dbd892b29e876a
laopo001 <353272497@qq.com>
2019-09-06 19:03:36 +0800

    fix pin

--- src/bin/pin.rs
+++ src/bin/pin.rs
@@ -2,14 +2,20 @@ use std::marker::PhantomPinned;
 use std::pin::Pin;
 use std::ptr::NonNull;
 
+#[derive(Debug)]
 struct Person {
     name: String,
 }
+impl Drop for Person {
+    fn drop(&mut self) {
+        println!("Dropping  Person`{}`!", self.name);
+    }
+}
 
+#[derive(Debug)]
 struct Class {
-    persons: [Person; 2],
+    persons: Vec<Person>,
     curr: NonNull<Person>,
-    _pin: PhantomPinned,
 }
 
 fn main() {
@@ -21,18 +27,18 @@ fn main() {
     };
     let class = Class {
         curr: NonNull::dangling(),
-        persons: [a, b],
-        _pin: PhantomPinned,
+        persons: vec![a, b],
     };
     let mut boxed = Box::pin(class);
     let curr_ref = NonNull::from(&boxed.persons[0]);
     unsafe {
-        let mut_ref: Pin<&mut Class> = Pin::as_mut(&mut boxed);
+        let mut_ref: Pin<&mut Class> = boxed.as_mut();
         Pin::get_unchecked_mut(mut_ref).curr = curr_ref;
     }
     unsafe {
-        (*boxed.curr.as_ptr()).name = "123123".to_string();
-        // dbg!(boxed.curr.as_ref().name);
+        (*boxed.curr.as_ptr()).name = "123".to_string();
+        // (boxed.as_mut().get_mut().curr.as_mut()).name = "1234".to_string();
+        println!("{:?}", boxed.as_mut().curr.as_ref());
         println!("{:?}", boxed.curr.as_ref().name);
     }
     println!("pin.");
@@ -44,5 +50,19 @@ fn main() {
     unsafe {
         (*ptr_c).name = "123123".to_string();
         println!("{:?}", (*ptr_c).name);
+        println!("{:?}", boxed);
     }
+
+    let d = Person {
+        name: "d".to_string(),
+    };
+    let mut pin_box_d = Box::pin(d);
+    drop(pin_box_d);
+
+    let e = Person {
+        name: "e".to_string(),
+    };
+    drop(e);
+
+    println!("==========================");
 }
