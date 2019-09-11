b7f831f1dfe67603f2c2013ce4e16e63fd17b832
laopo001 <353272497@qq.com>
2019-09-11 14:57:51 +0800

    'fix'

--- src/bin/memory_location2.rs
+++ src/bin/memory_location2.rs
@@ -1,3 +1,28 @@
-fn main() {
+#[derive(Debug)]
+struct T {
+    id: i32,
+}
 
+#[derive(Debug)]
+struct F {
+    t: T,
+    name: i32,
+}
+
+fn main() {
+    let mut c = F { t: T { id: 1 }, name: 2 };
+    let e = &mut c as *mut F;
+    let d = c;
+    unsafe {
+//        println!("{:?}", c);
+        println!("{:p},{:?}", e, *e);
+        println!("{:p},{:?}", &d, d);
+        println!("{:p}", &d.t.id);
+        (*e).t.id = 3;
+        (*e).name = 4;
+        println!("{:p},{:?}", e, *e);
+        println!("{:p},{:?}", &d, d);
+        println!("{:p}", &(*e).t.id);
+        println!("==========================");
+    }
 }
