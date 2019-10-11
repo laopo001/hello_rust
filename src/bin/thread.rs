a0582be1fb26434c6b41471b21b1325096fbc92d
laopo001 <353272497@qq.com>
2019-10-11 19:37:10 +0800

    fix

--- src/bin/thread.rs
+++ src/bin/thread.rs
@@ -1,9 +1,11 @@
-use std::thread;
-use std::sync::{Arc, mpsc, Mutex};
+use std::sync::{mpsc, Arc, Mutex};
+use std::{thread, time};
 
 fn main() {
     let mut i = 0;
     let j: Arc<Mutex<i32>> = Arc::new(Mutex::new(0));
+    println!("-------------");
+    thread::sleep(time::Duration::from_secs(3));
     {
         let k = j.clone();
         let child = thread::spawn(move || {
@@ -11,6 +13,7 @@ fn main() {
             let mut data = k.lock().unwrap();
             *data += 1;
             println!("{},{}", i, data);
+            thread::sleep(time::Duration::from_secs(3));
         });
         child.join();
     }
