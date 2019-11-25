ef57665549130fe9b976cce0cdf2a08281d8805f
laopo001 <353272497@qq.com>
2019-11-25 20:30:26 +0800

    fix

--- src/bin/thread2.rs
+++ src/bin/thread2.rs
@@ -1,13 +1,30 @@
 use std::sync::{mpsc, Arc, Mutex};
 use std::{thread, time};
 
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)()
+    }
+}
+
 fn main() {
     let v = vec![1, 2, 3];
     let v_a: Arc<Mutex<Vec<i32>>> = Arc::new(Mutex::new(v));
+    let mut q: Vec<i32> = vec![1, 2];
+    let a = move || {
+        q.push(3);
+        println!("{:?}", q);
+    };
+    let box_a = Box::new(a);
     let mut arr: Vec<thread::JoinHandle<()>> = Vec::new();
     {
         let k = v_a.clone();
         let child = thread::spawn(move || {
+            box_a.call_box();
             let mut data = k.lock().unwrap();
             data.push(4);
             println!("{:?}", data);
