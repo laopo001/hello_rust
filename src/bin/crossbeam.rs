9091df3fb8746ea34b707dbbd1a9fde75fd96bb5
laopo001 <353272497@qq.com>
2020-01-16 15:24:46 +0800

    add crossbeam-channel

---
+++ src/bin/crossbeam.rs
@@ -0,0 +1,23 @@
+use crossbeam_channel::unbounded;
+use std::{thread, time};
+
+fn main() {
+    // Create a channel of unbounded capacity.
+    let (s, r) = unbounded();
+    let (s2) = (s.clone());
+    let j = thread::spawn(move || {
+        // Send a message into the channel.
+        s.send("Hello, world!").unwrap();
+    });
+    j.join();
+    let j = thread::spawn(move || {
+        // Send a message into the channel.
+        s2.send("Hello, world!2").unwrap();
+    });
+    j.join();
+    let j = thread::spawn(move || {
+        assert_eq!(r.recv(), Ok("Hello, world!"));
+        assert_eq!(r.recv(), Ok("Hello, world!2"));
+    });
+    j.join();
+}
