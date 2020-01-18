28c9d21548a81eb75f9458e6055ed65828252bae
laopo001 <353272497@qq.com>
2020-01-18 14:51:18 +0800

    add drop

---
+++ src/bin/drop.rs
@@ -0,0 +1,19 @@
+struct A(i32);
+
+impl Drop for A {
+    fn drop(&mut self) {
+        println!("Dropping!");
+    }
+}
+
+fn test(a: A) {}
+
+fn main() {
+    let mut a = A(1);
+    let ptr_a = &mut a as &mut A as *mut A;
+    test(a);
+    unsafe {
+        (*ptr_a).0 = 2;
+        dbg!("123", (*ptr_a).0);
+    }
+}
