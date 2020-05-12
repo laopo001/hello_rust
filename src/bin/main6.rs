566439dd8b614a0f3bef66d42455c03d40b81b2b
laopo001 <353272497@qq.com>
2020-05-12 23:05:05 +0800

    fx

---
+++ src/bin/main6.rs
@@ -0,0 +1,24 @@
+#[derive(Debug)]
+struct A {
+  name:i32
+}
+#[derive(Debug)]
+struct B {
+  a:A
+}
+
+
+fn run(mut a:Box<A>){
+
+  a.name =123;
+  dbg!(a.name);
+}
+
+fn main() {
+  let  a = Box::new(A{name:0});
+   
+  run(a);
+ 
+  dbg!(a.name);
+}
+
