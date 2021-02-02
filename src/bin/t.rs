ae389248ea36873843a5070e40a4358bafe8936d
laopo001 <353272497@qq.com>
2021-02-02 19:51:07 +0800

    add dyn object Vec

---
+++ src/bin/t.rs
@@ -0,0 +1,38 @@
+trait Update {
+    fn update(&mut self);
+}
+#[derive(Debug)]
+struct A {
+    i: i32,
+}
+impl Update for A {
+    fn update(&mut self) {
+        self.i = 1;
+        dbg!(self);
+    }
+}
+#[derive(Debug)]
+struct B {
+    i: i32,
+}
+impl Update for B {
+    fn update(&mut self) {
+        self.i = 2;
+        dbg!(self);
+    }
+}
+
+enum Com {
+    Update(Box<dyn Update>),
+    Text(String),
+}
+
+fn main() {
+    let mut a = A { i: 0 };
+    let mut b = B { i: 0 };
+    Update::update(&mut a);
+    Update::update(&mut b);
+    let mut arr: Vec<Box<dyn Update>> = vec![];
+    arr.push(Box::new(a));
+    arr.push(Box::new(b));
+}
